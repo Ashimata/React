9e5ed400c2b727f92a6e2034804c18f7dd02b54b
Ashimata <127284265+Ashimata@users.noreply.github.com>
2023-03-29 15:16:34 +0800

    Add files via upload

---
+++ Hello World React/src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+class HelloWorld extends React.Component {
+render() {
+return (
+<div>
+<center><h1>Hello World!</h1></center>
+</div>
+);
+}
+}
+export default HelloWorld;
